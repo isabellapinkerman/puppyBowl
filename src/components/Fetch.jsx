7231419780fb7a58e8d28d20333b67745cb40742
Bella <Isabellapinkerman@gmail.com>
2022-10-13 11:47:36 -0400

    second fetch commit

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -4,12 +4,14 @@ const Fetch = (props) =>{
  useEffect(()=>{
     async function getData(){
         let response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2206-ftb-et-web-ft/players')
+        let jasonData = await response.json()
+        console.log(jasonData)
     }
     getData()
- ),[])
+   },[])
+
     return(
  <div></div>
-   
  )}
 
 export default Fetch;
