ae52cf6e3c40c8d978b606d41feea41f34127298
Brianna <yaniilovegirl0023@gmail.com>
2022-10-13 13:03:07 -0500

    fetch function is working

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
 const Fetch = (props) =>{
+    const [data, setData] = useState()
  useEffect(()=>{
     async function getData(){
         let response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2206-ftb-et-web-ft/players')
         let jasonData = await response.json()
         console.log(jasonData)
+       setData(jasonData)
     }
     getData()
    },[])
 
+   console.log(data)
+
     return(
- <div></div>
+ <div className= 'puppy'> {data} </div>
  )}
 
 export default Fetch;
