ee57ad965ec492719f87295f98a160a9fea23ffa
Brianna <yaniilovegirl0023@gmail.com>
2022-10-17 12:26:49 -0500

    Search Bar commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useState } from 'react';
 
 const SearchBar = (props)=> {
     const puppy = props.puppy
-    const [input,setInput]= useState(0);
-
+    const [input,setInput]= useState("");
+    
     function search(name, breed){
         if(name == input){
             return setInput(puppy)
